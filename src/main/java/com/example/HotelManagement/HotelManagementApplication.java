894e2e05651eca7184485abc8a4373252676ba15
Phong <phamkhacthanhphong@gmail.con>
2024-04-08 23:31:29 +0700

    ~fix

--- src/main/java/com/example/HotelManagement/HotelManagementApplication.java
+++ src/main/java/com/example/HotelManagement/HotelManagementApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class HotelManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HotelManagementApplication.class, args);
 	}
